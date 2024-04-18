37e8e9e6db761b999a1a01f5dffb8e1c0a5459c5
nickl474 <linder.nick@ymail.com>
2024-04-17 23:58:25 -0700

    code applied
    code

--- DACs_classification.py
+++ DACs_classification.py
@@ -1,14 +1,83 @@
 #Step 1:
-# Import libraries
-# In this section, you can use a search engine to look for the functions that will help you implement the following steps
+#Import libraries
+#pip install pandas, matplotlib, and scikit-learn
 
+import  numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
 
 #Step 2:
 # Load dataset and show basic statistics
-# 1. Show dataset size (dimensions)
-# 2. Show what column names exist for the 49 attributes in the dataset
-# 3. Show the distribution of the target class CES 4.0 Percentile Range column
-# 4. Show the percentage distribution of the target class CES 4.0 Percentile Range column
+dataset = pd.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[:, :-1]
+y = dataset.iloc[:, -1]
+
+#Split dataset in train and test splits
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+#Data preprocessing
+from sklearn.preprocessing import StandardScaler
+
+sc = StandardScaler()
+
+X_train = sc.fit_transform(X_train)
+X_test = sc.fit_transform(X_test)
+
+#Model Development (Support Vector Classification)
+from sklearn.svm import SVC
+classifier = SVC(kernel = 'rbf', random_state = 0)
+classifier.fit(X_train, y_train)
+
+#accuracy test
+print(classifier.predict(sc.transform([[30, 87000]])))
+
+from sklearn.metrics import confusion_matrix, accuracy_score
+y_pred = classifier.predict(X_test)
+
+cm = confusion_matrix(y_test, y_pred)
+
+print(cm)
+
+print(accuracy_score(y_test, y_pred))
+
+
+# Visualising the Training set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = sc.inverse_transform(X_train), y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 0.25),
+                     np.arange(start = X_set[:, 1].min() - 1000, stop = X_set[:, 1].max() + 1000, step = 0.25))
+plt.contourf(X1, X2, classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+              alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Support vector machine (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+
+# Visualising the Test set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = sc.inverse_transform(X_test), y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 0.25),
+                     np.arange(start = X_set[:, 1].min() - 1000, stop = X_set[:, 1].max() + 1000, step = 0.25))
+plt.contourf(X1, X2, classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Support vector machine (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
 
 
 # Step 3:
